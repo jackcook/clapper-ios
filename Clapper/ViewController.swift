21b10efbfd07214cdc3967e70c062a04149c9757
Jack Cook <hello@jackcook.nyc>
2016-04-16 22:30:38 -0400

    Moved HTTP server stuff to AppDelegate

--- Clapper/ViewController.swift
+++ Clapper/ViewController.swift
@@ -6,28 +6,11 @@
 //  Copyright © 2016 Jack Cook. All rights reserved.
 //
 
-import Swifter
 import UIKit
 
 class ViewController: UIViewController {
     
-    var server: HTTPServer!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        DDLog.addLogger(DDTTYLogger.sharedInstance())
-        
-        server = HTTPServer()
-        server.setType("_http._tcp.")
-        server.setPort(12345)
-        server.setConnectionClass(ClapperConnection)
-        
-        do {
-            try server.start()
-            print("Started HTTP server on port \(server.listeningPort())")
-        } catch let error as NSError {
-            print("Error starting HTTP server: \(error)")
-        }
     }
 }
